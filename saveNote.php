3502dee82074ad0bfad6cae1dadb1a7624ab6df7
medlasswed <mohamed.lasswed2001@gmail.com>
2022-04-02 13:59:38 +0000

    notes

---
+++ saveNote.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+if ( isset($_POST["note"]) || isset($_POST["title"])){
+    $note  = array ( $_POST["title"],date('l jS \of F Y h:i:s A'),$_POST["note"] ) ;
+    $_SESSION["notes"] []=$note;
+
+}
+header("location:index.html");
+
+?>
+© 2022 GitHub, Inc.
